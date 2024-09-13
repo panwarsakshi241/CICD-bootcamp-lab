85fb123322ac88b7ddbf0c66c04b3e62e89b7d4e
Sakshi Panwar <sapanwar@deloitte.com>
2024-09-13 21:29:53 +0530

    Comment added to app.py file

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+#This is ths Home Class.
 class Home(Resource):
     def get(self):
         return {'version': '1.0'}
